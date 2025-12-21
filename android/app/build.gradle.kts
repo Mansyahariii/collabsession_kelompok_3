ae1783ff26fbc676e7d63424bda63ff8dd82e55f
Mansyahariii <arifirmansyah350@gmail.com>
2025-12-21 21:23:13 +0700

    tambah dependencies dan fix notifikasi di android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,16 +20,18 @@ dependencies {
 
   // Add the dependencies for any other desired Firebase products
   // https://firebase.google.com/docs/android/setup#available-libraries
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 android {
-    namespace = "com.example.collabsession"
+    namespace = "com.company.act"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -38,7 +40,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.collabsession"
+        applicationId = "com.company.act"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
