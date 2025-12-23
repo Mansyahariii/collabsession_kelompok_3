fab253e185120f05b7f7d5dd1545315a2f566f16
Mansyahariii <arifirmansyah350@gmail.com>
2025-12-23 18:22:53 +0700

    penyesuaian pada alignment

--- lib/pages/splashscreen4.dart
+++ lib/pages/splashscreen4.dart
@@ -12,63 +12,59 @@ class Splashscreen4 extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Column(
+              SizedBox(
+                height: 500,
+                child: Center(
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/digital-nomad-working-near-beach.png",
+                    ),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Text(
+                "Share Event \nWith Friends",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                  height: 1.2,
+                ),
+              ),
+              Row(
                 children: [
-                  SizedBox(
-                    height: 500,
-                    child: Center(
-                      child: Image(
-                        image: AssetImage(
-                          "assets/images/digital-nomad-working-near-beach.png",
-                        ),
-                        fit: BoxFit.contain,
+                  _dot(),
+                  SizedBox(width: 5),
+                  _dot(),
+                  SizedBox(width: 5),
+                  _dot(),
+                  SizedBox(width: 5),
+                  _dot(isActive: true),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Skip',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  Text(
-                    "Share Event \nWith Friends",
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                      height: 1.2,
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(24),
+                      backgroundColor: Colors.black,
                     ),
-                  ),
-                  Row(
-                    children: [
-                      _dot(),
-                      SizedBox(width: 5),
-                      _dot(),
-                      SizedBox(width: 5),
-                      _dot(),
-                      SizedBox(width: 5),
-                      _dot(isActive: true),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Skip',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          shape: CircleBorder(),
-                          padding: EdgeInsets.all(24),
-                          backgroundColor: Colors.black,
-                        ),
-                        child: Icon(Icons.arrow_forward, color: Colors.white),
-                      ),
-                    ],
+                    child: Icon(Icons.arrow_forward, color: Colors.white),
                   ),
                 ],
               ),
