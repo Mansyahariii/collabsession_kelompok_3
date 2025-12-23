9373ec77de0487a8e330af1fbb064117f9651a46
Mansyahariii <arifirmansyah350@gmail.com>
2025-12-23 18:03:07 +0700

    wrap sizedbox dan text ke column

--- lib/pages/splashscreen4.dart
+++ lib/pages/splashscreen4.dart
@@ -11,25 +11,29 @@ class Splashscreen4 extends StatelessWidget {
           padding: const EdgeInsets.all(20.0),
           child: const Column(
             children: [
-              SizedBox(
-                height: 500,
-                child: Center(
-                  child: Image(
-                    image: AssetImage(
-                      "assets/images/digital-nomad-working-near-beach.png",
+              Column(
+                children: [
+                  SizedBox(
+                    height: 500,
+                    child: Center(
+                      child: Image(
+                        image: AssetImage(
+                          "assets/images/digital-nomad-working-near-beach.png",
+                        ),
+                        fit: BoxFit.contain,
+                      ),
                     ),
-                    fit: BoxFit.contain,
                   ),
-                ),
-              ),
-              Text(
-                "Share Event \nWith Friends",
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black87,
-                  height: 1.2,
-                ),
+                  Text(
+                    "Share Event \nWith Friends",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                      height: 1.2,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
