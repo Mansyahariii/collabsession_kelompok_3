2ce5915dc55c2598b2ea7b710896da901d2e9c67
kidev14 <adekatuasa2001@gmail.com>
2025-12-23 15:57:31 +0700

    membuat stl

---
+++ lib/pages/splashscreen3.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen3 extends StatelessWidget {
+  const Splashscreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
