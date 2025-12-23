d04d1253689736edb837a04882c2fca02478dc9f
kidev14 <adekatuasa2001@gmail.com>
2025-12-23 16:06:35 +0700

    membuat override

--- lib/pages/splashscreen3.dart
+++ lib/pages/splashscreen3.dart
@@ -7,6 +7,19 @@ class Splashscreen3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.white,
+      body : SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // tambahkan illustration splash screen//
+              Center()
+            ],
+
+          )
+      ),
+      )
     );
   }
 }
