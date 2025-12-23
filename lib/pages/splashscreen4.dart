55f1db13defa5429de896a6c7c57e451a146a0b4
Mansyahariii <arifirmansyah350@gmail.com>
2025-12-23 17:59:34 +0700

    menambahkan image dan text pada splashscreen 4

--- lib/pages/splashscreen4.dart
+++ lib/pages/splashscreen4.dart
@@ -9,7 +9,30 @@ class Splashscreen4 extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          child: const Column(children: []),
+          child: const Column(
+            children: [
+              SizedBox(
+                height: 500,
+                child: Center(
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/digital-nomad-working-near-beach.png",
+                    ),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Text(
+                "Share Event \nWith Friends",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                  height: 1.2,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
