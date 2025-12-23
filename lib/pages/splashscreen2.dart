24004858a49ca095c920d06c2b7449e2a0801c6e
cendyal-hub <sendyaja17@gmail.com>
2025-12-23 16:45:49 +0700

    menambahkan basic scaffold, safeArea, dan Coloumn layout

---
+++ lib/pages/splashscreen2.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen2 extends StatelessWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+          ],
+        ),
+      ),
+    ),
+    );
+  }
+}
