e55996bdaacb40e11d4163d8f17680a9cbca96c8
Mansyahariii <arifirmansyah350@gmail.com>
2025-12-27 16:13:46 +0700

    import package heroicons dan membuat appBar pada scaffold

--- lib/pages/admin/admin_dashboard.dart
+++ lib/pages/admin/admin_dashboard.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 
 class AdminDashboard extends StatelessWidget {
   const AdminDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Dashboard Admin')),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const HeroIcon(
+          HeroIcons.squares2x2,
+          style: HeroIconStyle.outline,
+          color: Colors.black,
+        ),
+        actions: const [
+          HeroIcon(
+            HeroIcons.megaphone,
+            style: HeroIconStyle.outline,
+            color: Colors.black,
+          ),
+        ],
+      ),
+
+      body: const Center(child: Text('Dashboard Admin')),
     );
   }
 }
