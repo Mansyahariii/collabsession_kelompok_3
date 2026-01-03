790c54c685b222d4089754a08c4789029db40b11
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-03 18:17:06 +0700

    menambahkan icon dan import hero icon

--- lib/pages/user/user_homepage.dart
+++ lib/pages/user/user_homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 
 class UserHomepage extends StatelessWidget {
   const UserHomepage({super.key});
@@ -8,11 +9,8 @@ class UserHomepage extends StatelessWidget {
     return SafeArea(
       child: ListView(
         children: [
-          Text("Selamat Datang!",
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
-          ),
+          Text("Selamat Datang!", style: TextStyle(fontSize: 20.0)),
+          HeroIcon(HeroIcons.moon, size: 24.0),
         ],
       ),
     );
