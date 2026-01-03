a158ac9381e856472b458290d0ea22526659ce45
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-03 17:26:24 +0700

    import heroicon untuk dashboard user dan membuat appBar dengan icon

--- lib/pages/user/user_dashboard.dart
+++ lib/pages/user/user_dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 
 class UserDashboard extends StatefulWidget {
   const UserDashboard({super.key});
@@ -20,7 +21,21 @@ class _UserDashboardState extends State<UserDashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('User Dashboard'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const HeroIcon(
+          HeroIcons.squares2x2,
+          style: HeroIconStyle.outline,
+          color: Colors.black,
+        ),
+        actions: const [
+          HeroIcon(
+            HeroIcons.megaphone,
+            style: HeroIconStyle.outline,
+            color: Colors.black,
+          ),
+          SizedBox(width: 16),
+        ],
       ),
     );
   }
