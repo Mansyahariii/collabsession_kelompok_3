b431b2a2fc1a98e952977093e519195b6a75dfb6
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-03 17:48:27 +0700

    menambahkan bottomNavigationBar pada dashboard user

--- lib/pages/user/user_dashboard.dart
+++ lib/pages/user/user_dashboard.dart
@@ -37,6 +37,36 @@ class _UserDashboardState extends State<UserDashboard> {
           SizedBox(width: 16),
         ],
       ),
+      body: _pages[_currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: HeroIcon(HeroIcons.rectangleStack),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: HeroIcon(HeroIcons.newspaper),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: HeroIcon(HeroIcons.userCircle),
+            label: '',
+          ),
+        ],
+      ),
     );
   }
 }
