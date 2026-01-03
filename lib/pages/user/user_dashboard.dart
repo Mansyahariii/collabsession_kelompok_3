89baa69c37a8f70e16c176e9678d882cf9aed1bc
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-03 17:24:51 +0700

    membuat dashboard user

--- lib/pages/user/user_dashboard.dart
+++ lib/pages/user/user_dashboard.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
-class UserDashboard extends StatelessWidget {
+class UserDashboard extends StatefulWidget {
   const UserDashboard({super.key});
 
   @override
+  State<UserDashboard> createState() => _UserDashboardState();
+}
+
+class _UserDashboardState extends State<UserDashboard> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const Center(child: Text('Dashboard User')),
+    const Center(child: Text('Events Page')),
+    const Center(child: Text('Profile Page')),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Dashboard User')),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Dashboard'),
+      ),
     );
   }
 }
