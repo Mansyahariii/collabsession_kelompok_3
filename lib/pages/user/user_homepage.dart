00f8ce058a8afe8f463abf0a7493054796777b48
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-03 18:12:53 +0700

    membuat safearea dan child listview

--- lib/pages/user/user_homepage.dart
+++ lib/pages/user/user_homepage.dart
@@ -5,6 +5,10 @@ class UserHomepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: ListView(
+        children: [],
+      ),
+    );
   }
 }
