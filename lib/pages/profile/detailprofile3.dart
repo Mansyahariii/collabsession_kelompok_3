d7b8d36f3abbd4e4d2d2e1d3716a8f9606e36cae
kidev14 <adekatuasa2001@gmail.com>
2026-01-04 16:34:37 +0700

    menambahkan tentang saya

--- lib/pages/profile/detailprofile3.dart
+++ lib/pages/profile/detailprofile3.dart
@@ -45,6 +45,7 @@ class Detailprofile3 extends StatelessWidget {
                           ),
                         ),
                       ),
+
                       Text(
                         "Rizky Adekatuasa",
                         style: TextStyle(
@@ -53,26 +54,30 @@ class Detailprofile3 extends StatelessWidget {
                           color: Colors.white,
                         ),
                       ),
-                      SizedBox(height: 8),
+                      SizedBox(height: 10),
                       Container(
                         padding: const EdgeInsets.symmetric(
                           horizontal: 12.0,
-                          vertical: 4.0,
+                          vertical: 5.0,
                         ),
                         decoration: BoxDecoration(
                           color: Colors.blue,
                           borderRadius: BorderRadius.circular(20.0),
                         ),
-                        child: Text(
-                          "Flutter Developer",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
+
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Flutter Developer",
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
                       ),
-                      SizedBox(height: 8.0),
+                      SizedBox(height: 20),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -80,20 +85,119 @@ class Detailprofile3 extends StatelessWidget {
                           SizedBox(width: 4),
                           Text(
                             "Lampung, Indonesia",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "Project",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "123",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  //fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 30,
+                            child: VerticalDivider(
+                              thickness: 1.0,
+                              color: Colors.black,
+                              width: 15.0,
                             ),
                           ),
-                          Text(
-                            "Followers",
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              color: Colors.white,
+                          Column(
+                            children: [
+                              Text(
+                                "Project",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "123",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  //fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 30,
+                            child: VerticalDivider(
+                              thickness: 1.0,
+                              color: Colors.black,
+                              width: 15.0,
                             ),
-                          )
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Project",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "123",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  //fontWeight: FontWeight.w700,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
-                      )
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              Card(
+                elevation: 8,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Tentang Saya",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      Text(
+                        'Saya adalah seseorang yang selalu berusaha berkembang dan belajar hal-hal baru setiap hari. Dalam keseharian, saya berusaha menjaga sikap positif, menghargai proses, dan fokus pada tujuan yang ingin saya capai. Saya senang mengeksplorasi hal-hal yang menarik minat saya, baik dalam bidang kreativitas, teknologi, maupun aktivitas yang membantu saya memahami diri sendiri dengan lebih baik. Dengan tekad dan rasa ingin tahu yang tinggi, saya berharap dapat terus melangkah maju, membangun pengalaman baru, serta memberikan kontribusi yang bermanfaat bagi lingkungan sekitar',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.grey[700],
+                        ),
+                      ),
                     ],
                   ),
                 ),
