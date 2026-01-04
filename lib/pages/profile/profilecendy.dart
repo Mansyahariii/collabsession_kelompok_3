377fbbc20ad1c2658f1a25d28d5e91632ba4c131
cendyal-hub <sendyaja17@gmail.com>
2026-01-04 20:18:39 +0700

    membuat struktur dasar

---
+++ lib/pages/profile/profilecendy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F5F5),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+          ],
+        ),
+      ),
+    );
+  }
+}
