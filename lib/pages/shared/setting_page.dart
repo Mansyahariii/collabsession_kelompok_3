d0d33d46d22591c51c0c2026900adc9353d7539f
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-04 16:11:07 +0700

    membuat static widget _settingCard

--- lib/pages/shared/setting_page.dart
+++ lib/pages/shared/setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 
 class Settingpage extends StatelessWidget {
   const Settingpage({super.key});
@@ -15,6 +16,29 @@ class Settingpage extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+  
+  static Widget _settingCard({
+    required HeroIcons icon,
+    required String title,
+    required String subtitle,
+    required VoidCallback onTap,
+  }) {
+    return Card(
+      elevation: 0,
+      margin: const EdgeInsets.only(bottom: 12),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+        side: BorderSide(color: Colors.grey.shade200),
+      ),
+      child: ListTile(
+        leading: HeroIcon(icon, size: 22),
+        title: Text(title),
+        subtitle: Text(subtitle),
+        trailing: const HeroIcon(HeroIcons.chevronRight, size: 18),
+        onTap: onTap,
+      ),
     );
   }
 }
