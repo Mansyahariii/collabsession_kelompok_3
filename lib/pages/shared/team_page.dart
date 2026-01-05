abcd64824e95fb10583256471b2e23ceb7aeb0b5
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-05 22:54:15 +0700

    membuat file team page untuk list card anggota

---
+++ lib/pages/shared/team_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TeamPage extends StatelessWidget {
+  const TeamPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
