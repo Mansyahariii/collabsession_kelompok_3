fe1ff9ac2d2731ba278e33fe7f82abb65a12dca2
Mison1212 <misonwenda20@gmail.com>
2026-01-05 14:22:40 +0700

    membuat scaffold pada detailprofile2

--- lib/pages/profile/detailprofile2.dart
+++ lib/pages/profile/detailprofile2.dart
@@ -5,6 +5,9 @@ class Detileprofile2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SingleChildScrollView(),
+    );
   }
 }
