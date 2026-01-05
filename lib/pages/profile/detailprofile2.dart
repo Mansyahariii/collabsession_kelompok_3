b4259b00be2f890fe4e0092711da8a7b208e1386
Mison1212 <misonwenda20@gmail.com>
2026-01-05 14:36:57 +0700

    Header melengkung dengan stack

--- lib/pages/profile/detailprofile2.dart
+++ lib/pages/profile/detailprofile2.dart
@@ -8,8 +8,25 @@ class Detileprofile2 extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey,
       body: SingleChildScrollView(child: Column(children: [Stack(
-        
-      )])),
+         clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 220,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF1E3A8A),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40),
+                    ),
+                  ),
+                ),
+              ]
+
+      )
+      ]
+      )
+      ),
     );
   }
 }
