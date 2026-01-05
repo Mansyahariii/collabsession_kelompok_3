bb4a2f2417833b90423e47facc1438aa625ad6f3
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-05 21:55:39 +0700

    membuat future list _fetchEvent, dan import models activity,  import supabase

--- lib/pages/shared/event_page.dart
+++ lib/pages/shared/event_page.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:heroicons/heroicons.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../models/activity.dart';
 
 class EventPage extends StatelessWidget {
   const EventPage({super.key});
+
+  Future<List<Activity>> _fetchEvents() async {
+    final data = await Supabase.instance.client
+        .from('activities')
+        .select()
+        .order('date', ascending: true);
+
+    return (data as List).map((e) => Activity.fromMap(e)).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
