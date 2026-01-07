a544f5997c98fad7069bada65aa5dc17ccf4da2b
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-07 19:55:23 +0700

    membuat admin dashboard

--- lib/pages/admin/admin_dashboard.dart
+++ lib/pages/admin/admin_dashboard.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:heroicons/heroicons.dart';
+import '../../models/activity.dart';
+import '../../services/activity_service.dart';
+import '../shared/event_page.dart';
+import '../shared/setting_page.dart';
+import 'admin_homepage.dart';
 
 class AdminDashboard extends StatefulWidget {
   const AdminDashboard({super.key});
@@ -12,73 +17,61 @@ class _AdminDashboardState extends State<AdminDashboard> {
   int _currentIndex = 0;
 
   final List<Widget> _pages = [
-    const Center(child: Text('Dashboard Admin')),
-    const Center(child: Text('Events Page')),
-    const Center(child: Text('Profile Page')),
+    AdminHomePage(),
+    EventPage(),
+    Settingpage(),
   ];
+
+  PreferredSizeWidget? _buildAppBar() {
+    if (_currentIndex == 2) return null;
+
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: const HeroIcon(
+        HeroIcons.squares2x2,
+        style: HeroIconStyle.outline,
+        color: Colors.black,
+      ),
+      actions: const [
+        HeroIcon(
+          HeroIcons.megaphone,
+          style: HeroIconStyle.outline,
+          color: Colors.black,
+        ),
+        SizedBox(width: 16),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: const HeroIcon(
-          HeroIcons.squares2x2,
-          style: HeroIconStyle.outline,
-          color: Colors.black,
-        ),
-        actions: const [
-          HeroIcon(
-            HeroIcons.megaphone,
-            style: HeroIconStyle.outline,
-            color: Colors.black,
-          ),
-          SizedBox(width: 16),
-        ],
-      ),
-
+      appBar: _buildAppBar(),
       body: _pages[_currentIndex],
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.black,
-        shape: const CircleBorder(),
-        child: const Icon(Icons.add, color: Colors.white, size: 30),
-      ),
-
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            top: BorderSide(color: Colors.grey.shade300, width: 0.5),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: HeroIcon(HeroIcons.rectangleStack),
+            label: '',
           ),
-        ),
-        child: BottomNavigationBar(
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          backgroundColor: Colors.white,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.black,
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-              icon: HeroIcon(HeroIcons.rectangleStack),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: HeroIcon(HeroIcons.newspaper),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: HeroIcon(HeroIcons.cog6Tooth),
-              label: '',
-            ),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: HeroIcon(HeroIcons.newspaper),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: HeroIcon(HeroIcons.cog6Tooth),
+            label: '',
+          ),
+        ],
       ),
     );
   }
