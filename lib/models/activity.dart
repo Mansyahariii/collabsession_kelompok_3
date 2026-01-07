9c157abb3150d2901dc20b98f2ec78f5af3567e9
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-08 00:34:05 +0700

    mengubah string ke int

--- lib/models/activity.dart
+++ lib/models/activity.dart
@@ -1,5 +1,5 @@
 class Activity {
-  final String id;
+  final int id;
   final String title;
   final String description;
   final DateTime date;
@@ -15,7 +15,7 @@ class Activity {
 
   factory Activity.fromMap(Map<String, dynamic> map) {
     return Activity(
-      id: map['id'].toString(),
+      id: map['id'],
       title: map['title'],
       description: map['description'],
       date: DateTime.parse(map['date']),
