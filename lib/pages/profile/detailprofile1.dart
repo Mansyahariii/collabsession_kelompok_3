9b060872ccedcd8eada7831f9841adf72a85ef03
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-08 19:32:50 +0700

    membuat variable untuk menampung foto profile dan background

--- lib/pages/profile/detailprofile1.dart
+++ lib/pages/profile/detailprofile1.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class ProfilePage1 extends StatelessWidget {
   const ProfilePage1({super.key});
 
+  final String bgAsset = "assets/images/bgpicture.jpg";
+  final String profileAsset = "assets/images/profilepicture.jpg";
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
