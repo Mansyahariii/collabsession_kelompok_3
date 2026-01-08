2ca80adeabfa020e4bad68b98a1b50b3163b117a
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-08 19:40:04 +0700

    wrap stack ke column

--- lib/pages/profile/detailprofile1.dart
+++ lib/pages/profile/detailprofile1.dart
@@ -11,68 +11,72 @@ class ProfilePage1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Stack(
-          clipBehavior: Clip.none,
+        child: Column(
           children: [
-            Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(bgAsset),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 16,
-              left: 20,
-              child: InkWell(
-                borderRadius: BorderRadius.circular(12),
-                onTap: () => Navigator.pop(context),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 200,
+                  width: double.infinity,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                      image: AssetImage(bgAsset),
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  child: const HeroIcon(HeroIcons.chevronLeft, size: 20),
                 ),
-              ),
-            ),
-            Positioned(
-              bottom: -55,
-              left: 25,
-              child: Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  Container(
-                    width: 120,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 5),
-                      image: DecorationImage(
-                        image: AssetImage(profileAsset),
-                        fit: BoxFit.cover,
+                Positioned(
+                  top: 16,
+                  left: 20,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(12),
+                    onTap: () => Navigator.pop(context),
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
                       ),
+                      child: const HeroIcon(HeroIcons.chevronLeft, size: 20),
                     ),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 8, right: 8),
-                    padding: const EdgeInsets.all(4),
-                    decoration: const BoxDecoration(
-                      color: Colors.blue,
-                      shape: BoxShape.circle,
-                    ),
-                    child: const Icon(
-                      Icons.check,
-                      color: Colors.white,
-                      size: 14,
-                    ),
+                ),
+                Positioned(
+                  bottom: -55,
+                  left: 25,
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 5),
+                          image: DecorationImage(
+                            image: AssetImage(profileAsset),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 8, right: 8),
+                        padding: const EdgeInsets.all(4),
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          Icons.check,
+                          color: Colors.white,
+                          size: 14,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
