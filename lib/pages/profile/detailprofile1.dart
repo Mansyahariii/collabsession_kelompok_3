674fb1c2287457d2f763da9b4a64168a8e2154f1
Mansyahariii <arifirmansyah350@gmail.com>
2026-01-08 19:34:45 +0700

    membuat scaffold dengan widget singlechildscrollview dan stack untuk foto profile, background, dan tombol navigasi

--- lib/pages/profile/detailprofile1.dart
+++ lib/pages/profile/detailprofile1.dart
@@ -8,6 +8,8 @@ class ProfilePage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(child: Stack(children: [])),
+    );
   }
 }
