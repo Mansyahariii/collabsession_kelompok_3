e6e1cc5648ccc9015ab0458b539c348c1bddf47e
kidev14 <adekatuasa2001@gmail.com>
2026-01-09 13:42:29 +0700

    menyesuaikan tampilan team page

--- lib/pages/shared/team_page.dart
+++ lib/pages/shared/team_page.dart
@@ -11,71 +11,82 @@ class TeamPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: [
-          const SizedBox(height: 40),
-          Row(
-            children: [
-              InkWell(
-                borderRadius: BorderRadius.circular(12),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  InkWell(
                     borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.grey.shade300),
+                    onTap: () => Navigator.pop(context),
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Colors.grey.shade300),
+                      ),
+                      child: const HeroIcon(HeroIcons.chevronLeft, size: 20),
+                    ),
                   ),
-                  child: const HeroIcon(HeroIcons.chevronLeft, size: 20),
-                ),
+                  const SizedBox(width: 12),
+                  const Text(
+                    'Tim Pengembang',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
-              const SizedBox(width: 12),
-              const Text(
-                'Tim Pengembang',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                children: [
+                  _TeamCard(
+                    name: 'Ari Firmansyah',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const ProfilePage1()),
+                      );
+                    },
+                  ),
+                  _TeamCard(
+                    name: 'Rizky Adekatuasa',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const Detailprofile3(),
+                        ),
+                      );
+                    },
+                  ),
+                  _TeamCard(
+                    name: 'Cendy Alvianto',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const ProfilePage()),
+                      );
+                    },
+                  ),
+                  _TeamCard(
+                    name: 'Mison Wenda',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const Detileprofile2(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          _TeamCard(
-            name: 'Ari Firmansyah',
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const ProfilePage1()),
-              );
-            },
-          ),
-          _TeamCard(
-            name: 'Rizky Adekatuasa',
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const Detailprofile3()),
-              );
-            },
-          ),
-          _TeamCard(
-            name: 'Cendy Alvianto',
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const ProfilePage()),
-              );
-            },
-          ),
-          _TeamCard(
-            name: 'Mison Wenda',
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const Detileprofile2()),
-              );
-            },
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
