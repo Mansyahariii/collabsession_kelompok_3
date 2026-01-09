79ab9c4cdb51405083a8492a6990d534d2577d40
kidev14 <adekatuasa2001@gmail.com>
2026-01-09 11:51:44 +0700

    membuat header dengan konten tombol kembali dan teks

--- lib/pages/shared/team_page.dart
+++ lib/pages/shared/team_page.dart
@@ -11,7 +11,36 @@ class TeamPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [])),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  InkWell(
+                    borderRadius: BorderRadius.circular(12),
+                    onTap: () => Navigator.pop(context),
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Colors.grey.shade300),
+                      ),
+                      child: const HeroIcon(HeroIcons.chevronLeft, size: 20),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  const Text(
+                    'Tim Pengembang',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
