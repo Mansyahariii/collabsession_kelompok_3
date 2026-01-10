786ad3b2083c629af1bbabd5e825b42019bf876a
cendyal-hub <sendyaja17@gmail.com>
2026-01-10 17:41:27 +0700

    menambahkan animasi slide up untuk menghilangkan splash

--- lib/pages/logo.dart
+++ lib/pages/logo.dart
@@ -27,27 +27,40 @@ class _SplashfullState extends State<Splashfull> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Stack(
         children: [
           const SplashScreen(),
-          Container(color: Colors.grey),
-          AnimatedOpacity(
-            opacity: showLogo ? 1.0 : 0.0,
-            duration: const Duration(milliseconds: 600),
-            child: SizedBox(
-              width: 140,
-              height: 140,
-              child: ClipOval(
-                child: Image.asset(
-                  'assets/images/logo_global.jpg',
-                  fit: BoxFit.cover,
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 700),
+            curve: Curves.easeInOut,
+            top: slideUp ? -height : 0,
+            left: 0,
+            right: 0,
+            bottom: slideUp ? height : 0,
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.grey),
+              child: Center(
+                child: AnimatedOpacity(
+                  opacity: showLogo ? 1.0 : 0.0,
+                  duration: const Duration(milliseconds: 600),
+                  child: SizedBox(
+                    width: 140,
+                    height: 140,
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/images/logo_global.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            )
-          )
-        ]
-        )
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
