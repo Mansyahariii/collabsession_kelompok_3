8c65ce173a7fa21b8a7f1dacfdadc1856dc6195b
cendyal-hub <sendyaja17@gmail.com>
2026-01-10 17:25:07 +0700

    membuat struktur dasar

---
+++ lib/pages/logo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splashfull extends StatefulWidget {
+  const Splashfull({super.key});
+
+  @override
+  State<Splashfull> createState() => _SplashfullState();
+}
+
+class _SplashfullState extends State<Splashfull> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
